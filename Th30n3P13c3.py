36683911aaad21174178cff303dac7b961882675
R1@2 <52790238+riaz34@users.noreply.github.com>
2022-08-10 00:25:46 +0600

    Update Th30n3P13c3.py
    re-work

--- Th30n3P13c3.py
+++ Th30n3P13c3.py
@@ -1,62 +1,68 @@
 import requests
+from termcolor import colored
 import socket 
 import threading
 import asyncio
 from time import sleep
 from playwright.async_api import async_playwright
 
-print("""\n1) Port scan
-2) Ssl check
-3) Security miscon
-4) Directory traversal
-5) Reflected xss
-6) Exit \n""")
+print(colored("""\n
+######################################
+# 1) Port scan.                      #
+# 2) Cryptographic failure.          #
+# 3) Security misconfiguration.      #
+# 4) Directory traversal.            #
+# 5) Reflected xss.                  #
+# 6) Exit                            #
+#                      MADE BY R1@2  #
+###################################### \n""",'magenta'))
+
 
 while True:
 
- number = int (input("enter the number: "))
+ number = int(input(colored("\n Enter the number: ",'magenta')))
 
  if number == 1 :
   try:
-   url= input("Enter the url: ")
+   url= input(colored("Enter the url: ",'magenta'))
 
    def port_scanner(port):
 
       try:
           s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
           s.connect((url, port))
-          print(f"Open port => {port}")  
-
+          print(colored(f"Open port => {port}","yellow"))
+            
       except:
           pass
 
 
    for port in range(0,65535):  
 
-     thread = threading.Thread(target =port_scanner, args=[port]) 
+     thread = threading.Thread(target = port_scanner, args=[port]) 
      thread.start() 
 
   except RuntimeError:
     pass   
 
- elif number == 2 : 
-  print("\nexample url :www.google.com\n")
-  url=input("Enter the url: ")
+ elif number == 2 :
+  print(colored("\n Example url : www.google.com\n",'magenta'))
+  url=input(colored("Enter the url: ",'magenta'))
   ht="https://"
   url1=ht+url
 
   try:
       response = requests.get(url1, verify=True)
-      print("Ssl certified")
+      print(colored('Not Found :)', 'green'))
 
   except requests.exceptions.ConnectionError:
-      print("Ssl not certified")
+      print(colored('Found!!!','red','on_red'))
 
  elif number == 3 :
   
-  print("\nexample url : https://www.google.com/\n")
+  print(colored("\n Example url : https://www.google.com/\n"))
 
-  url = input("Enter the url: ")
+  url = input(colored("Enter the url: ",'magenta'))
   domain = url
 
   file = open("config.txt")
@@ -74,14 +80,14 @@ while True:
       url = f"{domain}{cfile}"
 
       if response.status_code == 200:
-        print("Security misconfiguration found!",url)
+        print(colored(f"Found => {url}","red"))
         counter = counter+1
        
       else:
        
         pass
   if counter == 0:
-    print("Security misconfiguration not found")
+    print(colored("Not Found :)","green"))
   else:
     pass
 
@@ -90,7 +96,7 @@ while True:
   print("\nexample url : https://insecure-website.com/loadImage?filename=\n")
   
   counter1=0
-  url = input("Enter the url: ")
+  url = input(colored("Enter the url: ",'magenta'))
   urld = url
 
   file = open("dirt.txt")
@@ -106,7 +112,7 @@ while True:
       url = f"{urld}{cfile}"
 
       if response.status_code == 200:
-        print("Directory traversal found!",url)
+        print(colored(f"Found => {url}","red"))
        
         counter1=counter1+1
        
@@ -115,12 +121,13 @@ while True:
         pass
   
   if counter1 == 0:
-    print("Directory traversal not found")
+    print(colored("Not Found :)","green"))
   else:
     pass
 
  elif number == 5 :
-  urls = input("Enter the url: ")
+  print(colored("Copy the Url and paste",'magenta'))
+  urls = input(colored("Enter the url: ",'magenta'))
   async def run(playwright):
     chromium = playwright.chromium
     browser = await chromium.launch(headless=True)
@@ -142,9 +149,9 @@ while True:
     text_arr =await page.locator("//*[contains(text(),'Su1fur1cAc1d')]").all_text_contents()
     textlen=len(text_arr)
     if textlen>1:
-      print("might be vulnerable")
+      print(colored("might be vulnerable !!!","red","on_red"))
     else:
-      print("Not vulnerable")
+      print(colored("Not vulnerable :)","green"))
     
     sleep(2)
     await browser.close()
@@ -155,10 +162,9 @@ while True:
         
   asyncio.run(main())
 
-  
  elif number == 6:
   break   
   
  else:
-   print("\n please select the right number")
+   print(colored("\n Please select the right number",'red'))
 
